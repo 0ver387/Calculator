0d00b768c4fa62dd6136938a8453276bddffd8ac
0ver387 <0ver387@gmail.com>
2012-03-28 16:25:59 +0600

    1

--- Scripts/build_script.sh
+++ Scripts/build_script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
  
-echo ?We'll build later <img src="http://tutsplus.com/wp-content/themes/tuts-premium-theme/images/smilies/icon_smile.png" alt=":)" class="wp-smiley"> ?
+echo "We'll build later <img src="http://tutsplus.com/wp-content/themes/tuts-premium-theme/images/smilies/icon_smile.png" alt=":)" class="wp-smiley"> "
